71635c7cca78c4d6e9cc56b6dda1666a7ac81a6d
sainRaghuveer <raghuveersain987@gmail.com>
2023-04-29 14:25:34 +0530

    WCB

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const {client, Client} = require("whatsapp-web.js");
+const { Client } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-const {Configuration, OpenAIApi} = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 
 const client = new Client();
-client.on("qr", (qr)=>{
-    qrcode.generate(qr,{small:true});
+client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
 });
 
-client.on("ready",()=>{
+client.on("ready", () => {
     console.log("Client is ready");
+});
+
+client.initialize();
+
+const configuration = new Configuration({
+    apiKey: process.env.SECRET_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion(message) {
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: message,
+        max_tokens: 200,
+    });
+    return completion.data.choices[0].text;
+}
+
+client.on("message", message => {
+    console.log(message.body);
+    runCompletion(message.body).then(result => message.reply(result));
 })
+
